bd6d2dffc2f107fd05ada41810c9391f976d5efe
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-03-14 20:23:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# wrkspc
+
+> Tiny tool to save & restore npm / yarn workspace
+
+## Commands
+
+-   pack - Generate a lockfile of your npm / yarn workspace.
+-   unpack - Restore npm / yarn workspace from lockfile.
---
+++ index.mjs
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+import { mkdir, readFile, writeFile, access } from 'node:fs/promises';
+import { resolve, relative, join, dirname } from 'node:path';
+import process from 'node:process';
+
+import mapWorkspaces from '@npmcli/map-workspaces';
+import rpj from 'read-package-json-fast';
+import shebangRegex from 'shebang-regex';
+import sade from 'sade';
+
+const program = sade('wrkspc');
+
+program.describe('Pack & restore yarn / npm workspaces').version('0.0.0');
+
+program
+    .command('pack', 'Create workspace lockfile', { default: true })
+    .option('-d, --dir <path>', 'Path to the directory, containing npm/yarn workspace', process.cwd())
+    .option('--no-binaries', 'Disable binary file packaging')
+    .action(async (options) => {
+        await pack(options.dir, !options['no-binaries']);
+    });
+
+program.command('unpack', 'Restore workspace from lockfile').action(async () => {
+    await unpack();
+});
+
+const unpack = async () => {
+    const workspaceLockSource = await readFile(resolve(process.cwd(), 'workspace-lock.json'));
+    const workspaceLock = JSON.parse(workspaceLockSource.toString());
+
+    await Promise.all(
+        Object.entries(workspaceLock.packages).map(async ([_, value]) => {
+            const path = join(process.cwd(), value.path, 'package.json');
+            const parentDir = dirname(path);
+            try {
+                await access(parentDir);
+            } catch {
+                await mkdir(parentDir, { recursive: true });
+            }
+
+            await writeFile(path, JSON.stringify(value.package, null, 4));
+        }),
+    );
+
+    if (typeof workspaceLock.binaries === 'object') {
+        await Promise.all(
+            Object.entries(workspaceLock.binaries).map(async ([path, shebang]) => {
+                const fullPath = join(process.cwd(), path);
+                const parentDir = dirname(fullPath);
+                try {
+                    await access(parentDir);
+                } catch {
+                    await mkdir(parentDir, { recursive: true });
+                }
+
+                await writeFile(fullPath, shebang ?? '');
+            }),
+        );
+    }
+};
+
+const pack = async (workingDirectory, shouldParseBinaries) => {
+    const packageJson = await rpj(resolve(workingDirectory, 'package.json'));
+
+    const workspaces = await mapWorkspaces({
+        cwd: workingDirectory,
+        pkg: {
+            workspaces: packageJson.workspaces,
+        },
+    });
+
+    const workspaceQueue = Array.from(workspaces.entries());
+
+    const lockedPackageEntries = await Promise.all(
+        workspaceQueue.map(async ([key, value]) => [
+            key,
+            {
+                path: relative(workingDirectory, value),
+                package: await rpj(resolve(value, 'package.json')),
+            },
+        ]),
+    );
+
+    lockedPackageEntries.unshift(['', { path: '.', package: packageJson }]);
+
+    const lockedPackages = Object.fromEntries(lockedPackageEntries);
+
+    const workspaceLock = { packages: lockedPackages };
+
+    if (shouldParseBinaries) {
+        const binaryPaths = lockedPackageEntries
+            .map(([_, value]) => {
+                const bin = value.package.bin;
+
+                if (typeof bin !== 'object' || bin === null) {
+                    return undefined;
+                }
+
+                return Object.values(bin).map((binPath) => join(value.path, binPath));
+            })
+            .filter((value) => value !== undefined)
+            .flat();
+
+        const shebangs = await Promise.all(
+            binaryPaths.map(async (binaryPath) => {
+                const binaryContent = await readFile(binaryPath);
+
+                if (!shebangRegex.test(binaryContent)) {
+                    return [binaryPath, null];
+                }
+
+                return [binaryPath, shebangRegex.exec(binaryContent)[0]];
+            }),
+        );
+
+        workspaceLock.binaries = Object.fromEntries(shebangs);
+    }
+
+    await writeFile(join(workingDirectory, 'workspace-lock.json'), JSON.stringify(workspaceLock, null, 4));
+};
+
+program.parse(process.argv, {
+    unknown: (arg) => `Unknown argument ${arg}`,
+});
